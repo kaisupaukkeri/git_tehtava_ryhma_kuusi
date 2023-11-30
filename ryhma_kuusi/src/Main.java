f541c26d1671f6e00114a88f6e256283c46eba65
Jalomies <142224339+Jalomies@users.noreply.github.com>
2023-11-30 12:55:51 +0200

    Tehtävän runko ja luokan muodostimet
    - Lisytty Car-luokka ja sen arvot ja metodit.
    -Lisätty Car-luokkaan perusmuodostin ja parametrimuodostin.

--- ryhma_kuusi/src/Main.java
+++ ryhma_kuusi/src/Main.java
@@ -3,6 +3,49 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hello group six!");
+		
+		//testing constructors
+		Car car = new Car("Lada", "Samara", 30);
 	}
 
+}// Main class ends
+
+Class Car {
+	//Atributes
+	String brand;
+	String model;
+	int amountOfFuel
+	
+	//Default constructor
+	Car() 
+	{
+	    brand = "";
+	    model = "";
+	    amountOfFuel = 0;
+	    printData();
+	}
+	
+	//Parameterized constructor
+	Car(String b, String m, int f)
+	{
+	    brand = b;
+	    model = m;
+	    amountOfFuel = f;
+	    printData();
+	}
+	
+	//Methods
+	brake() {
+		//Code here
+	}
+	
+	accelerate() {
+		//Code here
+	}
+	
+	printData {
+		//Code here
+		//Prints Car class data
+	}
+	
 }
